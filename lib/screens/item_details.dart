e22a8ac1c368b1b8c04ad393dc01efe53fdcf67c
Israel Alves Rocha <israel_iarb@hotmail.com>
2023-05-09 13:48:20 -0300

    feat: generate qr code

--- lib/screens/item_details.dart
+++ lib/screens/item_details.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:property_app/constants/colors.dart';
 import 'package:property_app/controllers/item_controller.dart';
 import 'package:property_app/models/item_model.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:flutter/services.dart';
+import 'package:blue_thermal_printer/blue_thermal_printer.dart';
 
 class ItemDetailsScreen extends StatefulWidget {
   final String itemId;
@@ -27,6 +31,12 @@ class _ItemDetailsScreenState extends State<ItemDetailsScreen> {
     });
   }
 
+  Future<void> _copyToClipboard(String data) async {
+    await Clipboard.setData(ClipboardData(text: data));
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('Dados copiados para a área de transferência')));
+  }
+
   @override
   Widget build(BuildContext context) {
     if (_item == null) {
@@ -34,7 +44,18 @@ class _ItemDetailsScreenState extends State<ItemDetailsScreen> {
       return const Center(child: CircularProgressIndicator());
     }
 
-    // exibe os dados do item
+    // converte os dados do item em uma string
+    String itemDetails = 'Nome: ${_item.name}\n'
+        'Modelo: ${_item.model}\n'
+        'Serial: ${_item.serial}\n'
+        'Categoria: ${_item.category}\n'
+        'Tipo: ${_item.type}\n'
+        'Conservação: ${_item.conservation}\n'
+        'Nota fiscal: ${_item.nfe}\n'
+        'Data de emissão da NFe: ${_item.nfeDate}\n'
+        'Responsável: ${_item.responsibleName}';
+
+    // exibe os dados do item e um botão para copiar para a área de transferência
     return Scaffold(
       appBar: AppBar(
         title: const Text('Detalhes do Item'),
@@ -45,12 +66,28 @@ class _ItemDetailsScreenState extends State<ItemDetailsScreen> {
           Text('Nome: ${_item.name}'),
           Text('Modelo: ${_item.model}'),
           Text('Serial: ${_item.serial}'),
-          Text('Category: ${_item.category}'),
+          Text('Categoria: ${_item.category}'),
           Text('Tipo: ${_item.type}'),
           Text('Conservação: ${_item.conservation}'),
           Text('Nota fiscal: ${_item.nfe}'),
           Text('Data de emissão da NFe: ${_item.nfeDate}'),
           Text('Responsável: ${_item.responsibleName}'),
+          const SizedBox(height: 16.0),
+          Container(
+            color: CustomColor.customWhite,
+            child: Center(
+              child: QrImage(
+                data: itemDetails,
+                version: QrVersions.auto,
+                size: 200.0,
+              ),
+            ),
+          ),
+          const SizedBox(height: 16.0),
+          ElevatedButton(
+            onPressed: () => _copyToClipboard(itemDetails),
+            child: const Text('Copiar para a Área de Transferência'),
+          ),
         ],
       ),
     );
